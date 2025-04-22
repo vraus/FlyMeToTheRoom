707bba51dcf381916d20d9626132958e8e9151d7
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2025-04-22 18:30:36 +0200

    Base Setup of the project

---
+++ dtj_project/Source/dtj_project/dtj_project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "dtj_project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, dtj_project, "dtj_project" );
